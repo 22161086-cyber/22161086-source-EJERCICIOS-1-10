c30e064e7fe6e1ac39d7e5718eae5cbce0b01a3b
22161086-cyber <22161086@itoaxaca.edu.mx>
2026-07-07 07:59:25 -0600

    Agregar ejercicios 16 y 17

--- mi_portafolio/js/portafolio.js
+++
@@ -1,7 +0,0 @@
-// Mensaje de bienvenida en la consola del navegador
-console.log("¡Bienvenido al portafolio web de Litzi!");
-
-// Efecto opcional: Un saludo rápido al cargar la página
-window.onload = function() {
-    console.log("Todos los elementos visuales se han cargado correctamente.");
-};
